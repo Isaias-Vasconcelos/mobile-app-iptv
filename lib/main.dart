d5c76e98878353c47008182a1700674d92c31d8a
jaofreire <joao.gabriel18872@gmail.com>
2024-10-07 23:31:13 -0300

    criando classe MyApp

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:iptv_mobile/screens/introduction_screen_widget.dart';
-import 'package:iptv_mobile/screens/login_widgets/login_screen_widget.dart';
+import 'package:iptv_mobile/my_app.dart';
 
 void main() {
   runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'AlphaTV',
-      theme: ThemeData(
-        primarySwatch: Colors.deepPurple,
-        brightness: Brightness.dark,
-        useMaterial3: true,
-      ),
-      initialRoute: "/",
-      routes: {
-        "/": (context) => const IntroductionScreenWidget(),
-        "/login": (context) => const LoginScreenWidget()
-      },
-    );
-  }
 }
